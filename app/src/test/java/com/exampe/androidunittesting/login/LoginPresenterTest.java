e40dd678e9d24c82dd5e421b862baea1d05e6a3e
Mohale Molieleng <mjmolieleng@gmail.com>
2020-07-24 00:02:19 +0200

    added user email test for valid input

--- app/src/test/java/com/exampe/androidunittesting/login/LoginPresenterTest.java
+++ app/src/test/java/com/exampe/androidunittesting/login/LoginPresenterTest.java
@@ -1,24 +1,24 @@
 package com.exampe.androidunittesting.login;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class LoginPresenterTest {
 
     private LoginView viewMock;
     private LoginPresenter presenter;
+    private LoginModel userMock;
+
     @Before
     public void setUp() throws Exception {
         viewMock = mock(LoginView.class);
-        presenter = new LoginPresenter(viewMock, mock(LoginModel.class));
+        userMock = mock(LoginModel.class);
+        presenter = new LoginPresenter(viewMock, userMock);
     }
 
     @After
@@ -26,7 +26,7 @@ public class LoginPresenterTest {
     }
 
     @Test
-    public void updateUserEmail() {
+    public void updateUserEmail_with_bad_data_must_render_showEmailError() {
         presenter.updateUserEmail(null);
         Mockito.verify(viewMock, times(1)).showEmailError();
 
@@ -35,6 +35,18 @@ public class LoginPresenterTest {
     }
 
     @Test
-    public void updatePassword() {
+    public void updateEmail_with_correct_email_must_update_userEmail() {
+        presenter.updateUserEmail("doe@gmail.com");
+        Mockito.verifyNoInteractions(viewMock);
+        Mockito.verify(userMock).setEmail("doe@gmail.com");
+    }
+
+    @Test
+    public void updatePassword_with_bad_data_must_render_error() {
+        presenter.updatePassword(null);
+        Mockito.verify(viewMock, times(1)).showEmailError();
+
+        presenter.updatePassword(" ");
+        Mockito.verify(viewMock, times(1)).showEmailError();
     }
 }
